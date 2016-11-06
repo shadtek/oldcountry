7e44a1a16c6337e65145d57842c17040242208d8
shadtek <spencer.shattuck@gmail.com>
2016-11-06 12:48:37 -0700

    Node router install.

--- server.js
+++ server.js
@@ -2,11 +2,69 @@ var express = require('express');
 
 // app setup
 var app = express();
+var router = express.Router();
+var path = __dirname + '/public/';
 var port = 8080;
 
 // setup the app middlware
-app.use(express.static('./public'));
+// app.use(express.static('./public'));
+
+router.use(function (req,res,next) {
+  console.log("/" + req.method);
+  next();
+});
+
+router.get("/",function(req,res){
+  res.sendFile(path + "index.html");
+});
+
+router.get("/about",function(req,res){
+  res.sendFile(path + "about.html");
+});
+
+router.get("/contact",function(req,res){
+  res.sendFile(path + "contact.html");
+});
+
+app.use("/",router);
+
+app.use("*",function(req,res){
+  res.sendFile(path + "404.html");
+});
 
 app.listen(port, function() {
 	console.log('Server up and running on port: ', port);
 });
+
+// node router example:
+// var express = require("express");
+// var app = express();
+// var router = express.Router();
+// var path = __dirname + '/views/';
+
+// router.use(function (req,res,next) {
+//   console.log("/" + req.method);
+//   next();
+// });
+
+// router.get("/",function(req,res){
+//   res.sendFile(path + "index.html");
+// });
+
+// router.get("/about",function(req,res){
+//   res.sendFile(path + "about.html");
+// });
+
+// router.get("/contact",function(req,res){
+//   res.sendFile(path + "contact.html");
+// });
+
+// app.use("/",router);
+
+// app.use("*",function(req,res){
+//   res.sendFile(path + "404.html");
+// });
+
+// app.listen(3000,function(){
+//   console.log("Live at Port 3000");
+// });
