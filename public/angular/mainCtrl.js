3e9ddd90ce5f01df3898d5440699faf2eee2928e
Spencer Shattuck <spencer.shattuck@gmail.com>
2016-12-15 17:13:54 -0700

    Clean up mainCtrl.js and prep for more nav to be built for more home page content.

--- public/angular/mainCtrl.js
+++ public/angular/mainCtrl.js
@@ -1,12 +1,5 @@
 var app = angular.module('oldcountry');
 
 app.controller('mainCtrl', function($scope, $location){
-	$scope.go = function(path) {
-		$location.path(path)
-	}
 
-	$scope.scrollTo = function (scrollLocation) {
-		$location.hash(scrollLocation);
-		$anchorScroll();
-	}
 })
