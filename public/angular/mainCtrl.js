ab7efa2d76a5eb8a043ebfccc5f53a56eb7e602b
Spencer Shattuck <spencer.shattuck@gmail.com>
2016-12-21 15:46:28 -0700

    Add scroll link to logo/brand and remove unused code.

--- public/angular/mainCtrl.js
+++ public/angular/mainCtrl.js
@@ -6,15 +6,12 @@ app.run(['$anchorScroll', function($anchorScroll) {
 
 app.controller('mainCtrl', function($scope, $location, $anchorScroll, $timeout){
 
-	// $scope.scrollTo = function (scrollLocation) {
-	// 	$location.hash(scrollLocation);
-	// 	$anchorScroll();
-	// }
-
 	$scope.scrollTo = function(id) {
 		$location.path("/");
+
 		$timeout(function(){
 			$location.hash(id);
+			$location.url();
 			$anchorScroll();
 		}, 200);
 	}
