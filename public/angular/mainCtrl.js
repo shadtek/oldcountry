eb9ec2dbccc4534253dd525657a4c714d078d9c7
Spencer Shattuck <spencer.shattuck@gmail.com>
2016-12-21 15:34:13 -0700

    Improvement to nav and scroll so it works from other pages, not just home.

--- public/angular/mainCtrl.js
+++ public/angular/mainCtrl.js
@@ -4,11 +4,19 @@ app.run(['$anchorScroll', function($anchorScroll) {
   $anchorScroll.yOffset = $('nav');
 }])
 
-app.controller('mainCtrl', function($scope, $location, $anchorScroll){
+app.controller('mainCtrl', function($scope, $location, $anchorScroll, $timeout){
 
-	$scope.scrollTo = function (scrollLocation) {
-		$location.hash(scrollLocation);
-		$anchorScroll();
+	// $scope.scrollTo = function (scrollLocation) {
+	// 	$location.hash(scrollLocation);
+	// 	$anchorScroll();
+	// }
+
+	$scope.scrollTo = function(id) {
+		$location.path("/");
+		$timeout(function(){
+			$location.hash(id);
+			$anchorScroll();
+		}, 200);
 	}
 
 })
