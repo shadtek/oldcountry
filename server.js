0a1db28f14058bc085231a393babae2e3fd2996d
Spencer Shattuck <spencer.shattuck@gmail.com>
2017-01-03 13:05:04 -0700

    change to server for heroku.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 
-var port = process.env.EXPRESS_PORT || 3000;
+var port = process.env.PORT || 3000;
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
